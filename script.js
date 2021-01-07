67d2aa35fa08e98d58acba1e9a65700a37dc9d50
TylerCoates <Tyler.coates@ocsbstudent.ca>
2021-01-07 15:40:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("age").addEventListener("click", determainer);
+function determainer() {
+let counter= document.getElementById("age box").value;
+while (counter!=15){
+counter=prompt("Try again");}{
+prompt("You win");
+}}
